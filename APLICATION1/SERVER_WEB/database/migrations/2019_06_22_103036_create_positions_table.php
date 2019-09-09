5b2506cde33d4681c261640d7838445a85272032
AbelSantiagoCode <44154931+AbelSantiagoCode@users.noreply.github.com>
2019-09-09 20:38:40 +0200

    Add files via upload

---
+++ APLICATION1/SERVER_WEB/database/migrations/2019_06_22_103036_create_positions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('direction');
+            $table->integer('coordenateY');
+            $table->integer('coordenateX');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('positions');
+    }
+}
